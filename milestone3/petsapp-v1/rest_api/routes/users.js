719c4c2fff88b2126297ed51159d33e85829f80c
mrpeem <31452811+mrpeem@users.noreply.github.com>
2018-04-18 18:27:11 -0700

    commented route file

--- milestone3/petsapp-v1/rest_api/routes/users.js
+++ milestone3/petsapp-v1/rest_api/routes/users.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const router = express.Router();
-// const mongoose = require("mongoose");
-// const bcrypt = require("bcrypt");
-// const jwt = require("jsonwebtoken");
 
 const UsersController = require("../controllers/users");
+
+// Guo's code lists out the different types of requests and how to handle each one.
+// Ex. app.get('/users/:userid', (req, res) => { ... });
+//
+// In this file, I listed ONLY the different of requests the file will handle and where
+// to find the code for how to handle requests, but the actual code for handling requests
+// is not in this file. 
+//
+// This approach allows you to quickly glance at requests that the file handles. The code for
+// how to handle requests can be found in the corresponding functions (ex. "getAll" and "getUser")
+// in rest_api/controllers/users.js
+
 
 //GET a list of all usernames
 router.get('/', UsersController.getAll);
