94a819c11d957e4940830dcf8ed649b907005325
yooseokseo <yos017@ucsd.edu>
2018-05-11 12:00:53 -0700

    view profile button changes

--- milestone5/app/static_files/js/viewProfile.js
+++ milestone5/app/static_files/js/viewProfile.js
@@ -1,4 +1,6 @@
 $(document).ready(() => {
+
+  // when a user clicks "Edit" button, Edit => Save, Delete => Cancel
   $('#editProfile').click(()=>{
     $('#firstname').attr('disabled', false);
     $('#lastname').attr('disabled', false);
@@ -8,4 +10,29 @@ $(document).ready(() => {
     $('#deleteProfile').hide();
     $('#cancelProfile').removeClass('cancelProfile-close');
   })
+  $('#saveProfile').click(()=>{
+    console.log('save');
+    // should run Ajax call to edit profile
+
+
+
+    // after saving is done,
+    // Save => Edit , Cancel => Delete
+    $('#deleteProfile').show();
+    $('#cancelProfile').addClass('cancelProfile-close');
+    $('#editProfile').show();
+    $('#saveProfile').addClass('saveProfile-close');
+
+  })
+  $('#deleteProfile').click(()=>{
+    // should run Ajax call to delete profile
+
+
+    
+    // after deleting is done
+    // go back to previous page
+    backtopage();
+  })
+
+
 })
