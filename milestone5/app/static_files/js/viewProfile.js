3fec12e2af88762af8cc1437ea71510585796af7
yooseokseo <yos017@ucsd.edu>
2018-05-11 11:25:51 -0700

    view Profile change button

--- milestone5/app/static_files/js/viewProfile.js
+++ milestone5/app/static_files/js/viewProfile.js
@@ -5,5 +5,7 @@ $(document).ready(() => {
     $('#dob').attr('disabled', false);
     $('#editProfile').hide();
     $('#saveProfile').removeClass('saveProfile-close');
+    $('#deleteProfile').hide();
+    $('#cancelProfile').removeClass('cancelProfile-close');
   })
 })
